366217f929b016145848b92a0a2cfaac790ca64c
elvisowusu <owusuelvisgyasi@gmail.com>
2025-04-29 23:21:48 +0000

    Filter transactions by user ID in getTransactions controller

--- backend/modules/transactions/controllers/getTransactions.controller.js
+++ backend/modules/transactions/controllers/getTransactions.controller.js
@@ -3,7 +3,9 @@ const transactionModel = require("../../../models/transactions.model");
 
 const getTransactions = asyncHandler(async (req, res) => {
     
-   const transactions = await transactionModel.find({})
+    const transactions = await transactionModel.find({
+       user_id:req.user.id
+   })
 
     res.status(200).json({
         status: "success",

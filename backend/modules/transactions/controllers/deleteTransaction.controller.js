ccf48ccfa58bcfdf53a9a89a60d37ec32fdad96d
elvisowusu <owusuelvisgyasi@gmail.com>
2025-04-30 06:56:44 +0000

    fix: improve transaction deletion logic and ensure user balance adjustment

--- backend/modules/transactions/controllers/deleteTransaction.controller.js
+++ backend/modules/transactions/controllers/deleteTransaction.controller.js
@@ -1,13 +1,12 @@
 const asyncHandler = require("../../../handlers/asyncHandler");
 const transactionModel = require("../../../models/transactions.model");
-const validator = require("validator")
+const validator = require("validator");
+const userModel = require("../../../models/users.model");
 
 const deleteTransaction = asyncHandler(async (req, res) => {
-    const {
-        transactionId
-    } = req.params;
+    const { transactionId } = req.params;
 
-    // Check if transactionId is provided
+    // Validate transaction ID
     if (!transactionId || !validator.isMongoId(transactionId)) {
         return res.status(400).json({
             status: "failed",
@@ -15,20 +14,37 @@ const deleteTransaction = asyncHandler(async (req, res) => {
         });
     }
 
-    const transaction = await transactionModel.findOneAndDelete({
-        _id: transactionId
-    });
+    // Find the transaction
+    const transaction = await transactionModel.findOne({ _id: transactionId });
 
+    // Check if it exists
     if (!transaction) {
         return res.status(404).json({
             status: "failed",
             message: "Transaction not found",
         });
     }
+
+    // Adjust user balance if it's income
+    if (transaction.transaction_type === "income") {
+        await userModel.updateOne(
+            { _id: req.user.id },
+            {
+                $inc: {
+                    balance: transaction.amount * -1,
+                },
+            }
+        );
+    }
+
+    // Delete the transaction
+    await transactionModel.deleteOne({ _id: transactionId });
+
+    // Send success response
     res.status(200).json({
         status: "success",
         message: "Transaction deleted successfully",
-        transaction
-    })
-})
-module.exports = deleteTransaction
+    });
+});
+
+module.exports = deleteTransaction;
